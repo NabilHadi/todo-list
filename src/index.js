75775f9d3f11ac95b5a522f27f101e0c11dfc2ec
Nabil Hadi <napilfadl@gmail.com>
2023-02-15 09:26:12 +0200

    Create Todo and Project classes

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-console.log("Hello Todo");
+import { v4 as uuid } from "uuid";
+
+class Todo {
+  constructor(title = "", description = "", dueDate = "", priority = 0) {
+    this.id = uuid();
+    this.title = title;
+    this.description = description;
+    this.dueDate = dueDate;
+    this.priority = priority;
+  }
+}
+
+class Project {
+  constructor(name = "", todos = []) {
+    this.id = uuid();
+    this.name = name;
+    this.todos = todos;
+  }
+}
+
+const myTodo = new Todo("test", "desc", "tmw", 0);
+console.log(myTodo);
