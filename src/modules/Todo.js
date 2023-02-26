15e1cf2101e7d0508236e7e6239e2b3b0d32a273
Nabil Hadi <napilfadl@gmail.com>
2023-02-26 08:41:56 +0200

    Change Todo class constructor to accept an object instead

--- src/modules/Todo.js
+++ src/modules/Todo.js
@@ -1,7 +1,7 @@
 import { v4 as uuid } from "uuid";
 
 export default class Todo {
-  constructor(title = "", description = "", dueDate = "", priority = 0) {
+  constructor({ title = "", description = "", dueDate = "", priority = 0 }) {
     this.id = uuid();
     this.title = title;
     this.description = description;
